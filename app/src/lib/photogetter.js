e91a0cdc812c4d5813defd8fe97db3327c37943a
Kenny Jackelen <kennyjackelen@gmail.com>
2016-06-11 16:31:44 -0500

    fix saving full size images when we don't need to rotate, add debug output

--- app/src/lib/photogetter.js
+++ app/src/lib/photogetter.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 var request = require('request');
 
 var CACHE_DIR = '/data/photocache';
+var DEBUG_MODE = false;
 
 var drive;
 
@@ -13,13 +14,25 @@ function initialize( myDrive ) {
   return getOnePhoto;
 }
 
-function getOnePhoto( id, w, h, callback ) {
+function getOnePhoto( id, w, h, origCallback ) {
   // This function gets the specified photo at the specified dimensions.
   // It will check the local disk cache first, and pull the photo from Google
   // if it can't find a suitable version locally.
   // This function is the preferred way to get a photo by ID.
   var isFullImage;
   var fileSuffix;
+
+  var callback = function() {
+    if ( DEBUG_MODE ) { 
+      console.log('ending:   ' + id + ' ' + w + 'x' + h );
+    }
+    origCallback.apply(this,arguments);
+  };
+
+  if ( DEBUG_MODE ) { 
+    console.log('starting: ' + id + ' ' + w + 'x' + h );
+  }
+
   if ( w === 0 && h === 0 ) {
     isFullImage = true;
     fileSuffix = 'full';
@@ -30,12 +43,20 @@ function getOnePhoto( id, w, h, callback ) {
   var filenameResized = CACHE_DIR + '/' + id + '-' + fileSuffix + '.jpg';
   var filenameFull = CACHE_DIR + '/' + id + '-full.jpg';
 
-  fs.access( filenameResized, fs.R_OK, _canAccessResizedFile );
+  if ( isFullImage ) {
+    fs.access( filenameFull, fs.R_OK, _canAccessFullSizeFile );
+  }
+  else {
+    fs.access( filenameResized, fs.R_OK, _canAccessResizedFile );
+  }
 
   function _canAccessResizedFile( err ) {
     if ( err ) {
       // The resized image didn't exist on disk.
       // See if the full size image is on disk instead.
+      if ( DEBUG_MODE ) { 
+        console.log( '[_canAccessResizedFile]: ' + err );
+      }
       _lookForFullSizeOnDisk();
       return;
     }
@@ -47,6 +68,9 @@ function getOnePhoto( id, w, h, callback ) {
     if ( err ) {
       // The resized image didn't exist on disk.
       // See if the full size image is on disk instead.
+      if ( DEBUG_MODE ) { 
+        console.log( '[_gotResizedFile]: ' + err );
+      }
       _lookForFullSizeOnDisk();
       return;
     }
@@ -55,11 +79,14 @@ function getOnePhoto( id, w, h, callback ) {
   }
 
   function _lookForFullSizeOnDisk() {
-    fs.access( filenameResized, fs.R_OK, _canAccessFullSizeFile );
+    fs.access( filenameFull, fs.R_OK, _canAccessFullSizeFile );
   }
 
   function _canAccessFullSizeFile( err ) {
     if ( err ) {
+      if ( DEBUG_MODE ) { 
+        console.log( '[_canAccessFullSizeFile]: ' + err );
+      }
       // The full size image didn't exist on disk.
       // Get it from the web.
       _getPhotoFromWeb( id, _gotPhotoFromWeb );
@@ -72,6 +99,9 @@ function getOnePhoto( id, w, h, callback ) {
 
   function _gotFullSizeFile( err, data ) {
     if ( err ) {
+      if ( DEBUG_MODE ) { 
+        console.log( '[_gotFullSizeFile]: ' + err );
+      }
       // The full size image didn't exist on disk.
       // Get it from the web.
       _getPhotoFromWeb( id, _gotPhotoFromWeb );
@@ -79,15 +109,19 @@ function getOnePhoto( id, w, h, callback ) {
     }
     // The full size image already existed on disk, but the resized image didn't.
     // Resize the full one, then save it to disk and send it back.
-    formatter.resize( data, w, h, _writeToFile );
+    if ( isFullImage ) {
+      callback( null, data );
+    }
+    else {
+      formatter.resize( data, w, h, _writeToFile );
+    }
   }
 
   function _getPhotoFromWeb( id, callback ) {
     var options = { url: 'https://docs.google.com/uc?id=' + id, encoding: null };
-    console.log('sending web request for photo with id: ' + id );
     request( options, function (error, response, body) {
       if (!error && response.statusCode === 200) {
-        _rotateImage( id, body, callback );
+        _rotateAndSaveFullsizeImage( id, body, callback );
         return;
       }
       callback( 'Error loading photo from web: ' + error );
@@ -119,9 +153,8 @@ function getOnePhoto( id, w, h, callback ) {
     formatter.save( imageBuffer, filenameResized, callback );
   }
 
-  function _rotateImage( id, buffer, callback ) {
+  function _rotateAndSaveFullsizeImage( id, buffer, callback ) {
     drive.getPhotoObject( id, __gotPhotoObject );
-    buffer = null;  // release reference to avoid memory leak
 
     function __gotPhotoObject( err, photo ) {
       if ( err ) {
@@ -129,11 +162,12 @@ function getOnePhoto( id, w, h, callback ) {
         return;
       }
       if ( !drive.needsRotation( photo ) ) {
-        callback( null, buffer );
+        formatter.save( buffer, filenameFull, callback );
       }
       else {
         formatter.rotate( buffer, __rotatedImage );
       }
+      buffer = null;  // release reference to avoid memory leak
     }
 
     function __rotatedImage( err, buffer ) {
@@ -142,6 +176,7 @@ function getOnePhoto( id, w, h, callback ) {
         return;
       }
       formatter.save( buffer, filenameFull, callback );
+      buffer = null;  // release reference to avoid memory leak
     }
   }
 }
